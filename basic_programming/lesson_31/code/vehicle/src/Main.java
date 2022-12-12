1b88d1cc44075749871e2689c5c45af3774bc2b9
Alisher Khamidov <a.a.khamidov@yandex.ru>
2022-12-13 01:01:01 +0300

    lesson_31

---
+++ basic_programming/lesson_31/code/vehicle/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+//        Car bmw = new Car();
+//        System.out.println(bmw.getNumberOfWheels());
+        Car maserati = new Car(
+                true,
+                4,
+                320,
+                2,
+                1900);
+        System.out.println(maserati);
+
+    }
+}
