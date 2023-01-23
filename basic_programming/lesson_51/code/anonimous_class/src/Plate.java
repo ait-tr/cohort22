78f8b12a2c8c1434c4fa3047d5a4fe84f6f43647
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-01-23 23:35:57 +0100

    lesson_51

---
+++ basic_programming/lesson_51/code/anonimous_class/src/Plate.java
@@ -0,0 +1,17 @@
+public class Plate {  // кухонная тарелка
+    String title;
+    int diameter;
+    String type;
+    String color;
+
+    public void displayInfo(){
+        System.out.println(title + " " + diameter);
+    }
+
+    public Plate(String title, int diameter, String type, String color) {
+        this.title = title;
+        this.diameter = diameter;
+        this.type = type;
+        this.color = color;
+    }
+}
